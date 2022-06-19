d452743fb37fc4b1e9d50d530f7a6c94ebaa33f8
Stefan Novitovic <rada.pet22@gmail.com>
2022-06-19 16:35:17 +0200

    log message if env variable value is not provided

--- app/kafka_configuration.py
+++ app/kafka_configuration.py
@@ -7,32 +7,32 @@ logging.basicConfig(level=logging.INFO)
 class KafkaConfig(object):
     @property 
     def kafka_service_host(self):
-        return os.getenv('KAFKA_SERVICE_INTERNAL_HOST')
+        return os.getenv('KAFKA_SERVICE_INTERNAL_HOST') or logging.info('KAFKA_SERVICE_INTERNAL_HOST not defined')
     
     @property 
     def kafka_service_port(self):
-        return os.getenv('KAFKA_SERVICE_INTERNAL_PORT')
+        return os.getenv('KAFKA_SERVICE_INTERNAL_PORT') or logging.info('KAFKA_SERVICE_INTERNAL_PORT not defined')
     
     @property 
     def kafka_service_input_topic(self):
-        return os.getenv('KAFKA_SERVICE_INPUT_TOPIC')
+        return os.getenv('KAFKA_SERVICE_INPUT_TOPIC') or logging.info('KAFKA_SERVICE_INPUT_TOPIC not defined')
     
     @property 
     def kafka_service_output_topic(self):
-        return os.getenv('KAFKA_SERVICE_OUTPUT_TOPIC')
+        return os.getenv('KAFKA_SERVICE_OUTPUT_TOPIC') or logging.info('KAFKA_SERVICE_OUTPUT_TOPIC not defined')
     
     @property
     def kafka_service_group_id(self):
-        return os.getenv('KAFKA_SERVICE_GROUP_ID')
+        return os.getenv('KAFKA_SERVICE_GROUP_ID') or logging.info('KAFKA_SERVICE_GROUP_ID not defined')
 
     @property
     def kafka_service_username(self):
-        return os.getenv('KAFKA_SERVICE_USERNAME')
+        return os.getenv('KAFKA_SERVICE_USERNAME') or logging.info('KAFKA_SERVICE_USERNAME not defined')
 
     @property
     def kafka_service_password(self):
-        return os.getenv('KAFKA_SERVICE_PASSWORD')
+        return os.getenv('KAFKA_SERVICE_PASSWORD') or logging.info('KAFKA_SERVICE_PASSWORD not defined')
     
     @property
     def kafka_auto_offset_reset(self):
-        return os.getenv('KAFKA_AUTO_OFFSET_RESET')
+        return os.getenv('KAFKA_AUTO_OFFSET_RESET') or logging.info('KAFKA_AUTO_OFFSET_RESET not defined')
